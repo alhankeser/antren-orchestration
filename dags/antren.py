dcfa071fd786b11697f01fdcdbeccff4e0e41084
Alhan Keser <alhankeser@gmail.com>
2024-02-18 09:52:41 +0100

    Parametrize region and connection id

--- dags/antren.py
+++ dags/antren.py
@@ -15,6 +15,9 @@ PROJECT_ID = os.getenv("GOOGLE_CLOUD_PROJECT_ID")
 BUCKET_NAME = os.getenv("CLOUD_STORAGE_BUCKET")
 DATASET_NAME = os.getenv("BIGQUERY_DATASET_NAME")
 TABLE_NAME = os.getenv("BIGQUERY_TABLE_NAME")
+GOOGLE_CLOUD_LOCATION = os.getenv("GOOGLE_CLOUD_LOCATION")
+GOOGLE_CLOUD_CONNECTION_ID = os.getenv("GOOGLE_CLOUD_CONNECTION_ID")
+
 
 with DAG(
     "antren",
@@ -24,30 +27,31 @@ with DAG(
 ) as dag:
     get_latest_activities = CloudRunExecuteJobOperator(
         task_id="get_latest_activities",
-        project_id=PROJECT_ID,
         job_name="app",
-        region="europe-west9",
-        gcp_conn_id="gcp",
+        project_id=PROJECT_ID,
+        region=GOOGLE_CLOUD_LOCATION,
+        gcp_conn_id=GOOGLE_CLOUD_CONNECTION_ID,
     )
 
     load_gcs_to_bigquery = GCSToBigQueryOperator(
         task_id="load_gcs_to_bigquery",
-        bucket=BUCKET_NAME,
         source_objects=["*.parquet"],
         source_format="PARQUET",
         skip_leading_rows=1,
         destination_project_dataset_table=f"{DATASET_NAME}.{TABLE_NAME}",
         write_disposition="WRITE_TRUNCATE",
-        location="europe-west9",
-        gcp_conn_id="gcp",
+        project_id=PROJECT_ID,
+        bucket=BUCKET_NAME,
+        location=GOOGLE_CLOUD_LOCATION,
+        gcp_conn_id=GOOGLE_CLOUD_CONNECTION_ID,
     )
 
     run_dbt = CloudRunExecuteJobOperator(
         task_id="run_dbt",
-        project_id=PROJECT_ID,
         job_name="dbt",
-        region="europe-west9",
-        gcp_conn_id="gcp",
+        project_id=PROJECT_ID,
+        region=GOOGLE_CLOUD_LOCATION,
+        gcp_conn_id=GOOGLE_CLOUD_CONNECTION_ID,
     )
 
     get_latest_activities >> load_gcs_to_bigquery >> run_dbt
